05fe3b76044ee50bf83ae136985e8d842b83d74a
Maciej Malinowski <105872023+mejsiejdev@users.noreply.github.com>
2022-11-29 11:28:21 +0100

    feat: :construction: Started working on a banner
    I still have to add "Get started" button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+import BannerImage from "../public/images/banner.jpeg";
+
+const Banner = () => (
+  <div className="relative w-full">
+    <div className="absolute w-full h-full flex flex-col items-center justify-center bg-black/25 2xl:rounded-lg">
+      <p className="text-white text-5xl font-bold">
+        Reserve the destinations of your dreams in no time.
+      </p>
+    </div>
+    <Image
+      alt="Banner"
+      src={BannerImage}
+      placeholder="blur"
+      className="block 2xl:rounded-lg"
+    />
+  </div>
+);
+
 export default function Home() {
   return (
-    <main className="h-full w-full grid justify-items-center items-center p-8">
-      <p className="text-3xl font-semibold text-gray-500 animate-pulse">Waiting to be implemented...</p>
+    <main className="h-full w-full flex flex-col items-center container">
+      <Banner />
     </main>
   );
 }
