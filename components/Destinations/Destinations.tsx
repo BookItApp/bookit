e15dd46427f441b930e44a7ed306a1a187bd0061
marcel-alefierowicz <mrclalefierowicz@gmail.com>
2022-12-06 11:29:54 +0100

    BOOK-5: Added a small text tag

--- components/Destinations/Destinations.tsx
+++ components/Destinations/Destinations.tsx
@@ -1,13 +1,17 @@
-import Link from "next/link"
+import Link from "next/link";
 import Card from "../Card";
-import Cartel from '../../public/images/cartel.png'
+import Cartel from "../../public/images/cartel.png";
 
 const Destinations = () => (
-    <div className="flex mt-4 p-4 w-full items-center justify-center rounded-lg">
-        <Card title={'Medeliin'} img={Cartel} alt={'medeliin'}/>
-        <Card title={'Medeliin'} img={Cartel} alt={'medeliin'}/>
-        <Card title={'Medeliin'} img={Cartel} alt={'medeliin'}/>]
+  <div className="w-full">
+      <h2 className="font-semibold text-xl">Popular Destinations</h2>
+    <div className="flex justify-between mt-4  w-full items-center rounded-lg">
+      <Card title={"Medeliin"} img={Cartel} alt={"medeliin"} />
+      <Card title={"Medeliin"} img={Cartel} alt={"medeliin"} />
+      <Card title={"Medeliin"} img={Cartel} alt={"medeliin"} />
+      <Card title={"Medeliin"} img={Cartel} alt={"medeliin"} />
+      <Card title={"Medeliin"} img={Cartel} alt={"medeliin"} />
     </div>
-)
-
+  </div>
+);
 export default Destinations;
