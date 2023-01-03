913f8a55d63fc18d75deaa24204b778a60b60c9a
Maciej Malinowski <mejsiejdev@gmail.com>
2023-01-03 08:59:34 +0100

    Nagłówek teraz wyświetla informację że jest się zalogowanym

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,8 @@
-import { getToken } from "next-auth/jwt";
 import Link from "next/link";
+import { cookies } from "next/headers"
 
-const Header = () => {
+const Header = async () => {
+  const loggedIn = cookies().has("next-auth.session-token")
   return (
     <header className="w-full p-4">
       <div className="container mx-auto flex flex-row justify-between items-center">
@@ -13,18 +14,24 @@ const Header = () => {
         </Link>
         <span className="visible sm:hidden font-icons text-4xl">menu</span>
         <div className="hidden sm:flex font-semibold flex-row gap-4">
-          <Link
-            href="/signin"
-            className="py-1 px-4 rounded-full hover:bg-gradient-to-br from-emerald-400/20 to-blue-600/20 transition"
-          >
-            Sign in
-          </Link>
-          <Link
-            href="/signup"
-            className="bg-gradient-to-br from-emerald-400 to-blue-600 py-1 px-4 rounded-full text-white transition hover:shadow-md"
-          >
-            Sign Up
-          </Link>
+          {!loggedIn ?
+            <>
+              <Link
+                href="/signin"
+                className="py-1 px-4 rounded-full hover:bg-gradient-to-br from-emerald-400/20 to-blue-600/20 transition"
+              >
+                Sign in
+              </Link>
+              <Link
+                href="/signup"
+                className="bg-gradient-to-br from-emerald-400 to-blue-600 py-1 px-4 rounded-full text-white transition hover:shadow-md"
+              >
+                Sign Up
+              </Link>
+            </>
+            :
+            <div className="flex flex-row items-center gap-2"><span className="font-icons text-3xl">person</span><p>Signed in</p></div>
+          }
         </div>
       </div>
     </header>
